c14390ccb36f3e54a73259aedc9c808abf7b8cb5
Imo David <emperorjagz@gmail.com>
2022-12-14 12:10:55 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Typography fontSize={["1rem", "5rem"]} fontWeight={300}>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Typography>
         <a
           className="App-link"
           href="https://reactjs.org"
