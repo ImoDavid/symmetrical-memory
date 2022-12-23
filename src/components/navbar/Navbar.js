7255a3929842b390de84db275c3604db493eae83
Imo David <emperorjagz@gmail.com>
2022-12-24 00:01:25 +0100

    feat: navbar responsiveness fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Box, styled, Avatar, Stack, Link, Typography } from "@mui/material";
 import { colors } from "../../styles/globals";
 import LOGO from "../../assets/logo.jpg";
@@ -16,6 +17,19 @@ const navLinks = [
   { page: "contact", url: "/contact" },
 ];
 export { navLinks };
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xxs: 0, // small phone
+      xs: 300, // phone
+      sm: 600, // tablets
+      md: 900, // small laptop
+      lg: 1200, // desktop
+      xl: 1536, // large screens
+    },
+  },
+});
 
 const StyledContainer = styled(Box)({
   backgroundColor: colors.PRIMARY,
@@ -35,7 +49,7 @@ const StyledNavLink = styled(Box)({
   fontSize: ["1rem", "0.8rem"],
   cursor: "pointer",
   marginRight: "1.8rem",
-  marginBottom:["0rem","0.5rem"]
+  marginBottom: ["0rem", "0.5rem"],
 });
 
 const StyledLink = styled(NavLink)({
@@ -53,67 +67,61 @@ const StyledMobileContainer = styled(Box)({
   backgroundColor: colors.WHITE,
   top: "4.9rem",
   left: 0,
-  padding:"1rem 0 "
+  padding: "1rem 0 ",
 });
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <StyledContainer paddingY={".5rem"} paddingX={["1rem", "3rem"]}>
-      <Box
-        bgcolor={colors.WHITE}
-        width={["90%", "80%"]}
-        marginX={"auto"}
-        padding={"1rem"}
-        borderRadius={"50px"}
-      >
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={["space-between", "flex-start"]}
-          paddingX={["1rem", "3rem"]}
-          position={"relatve"}
+    <ThemeProvider theme={theme}>
+      <StyledContainer paddingY={".5rem"} paddingX={["1rem", "0rem"]}>
+        <Box
+          bgcolor={colors.WHITE}
+          width={["90%", "80%"]}
+          marginX={"auto"}
+          padding={"1rem"}
+          borderRadius={"50px"}
         >
-          <Box
-            width={["auto", "16.779%"]}
-            display={"flex"}
+          <Stack
+            direction={"row"}
             alignItems={"center"}
-            justifyContent={"center"}
+            paddingX={["1rem", "0rem"]}
+            position={"relatve"}
+            sx={{
+              justifyContent: {
+                xs: "space-between",
+                sm: "space-between",
+                md: "space-between",
+                lg: "flex-start",
+                xl: "flex-start",
+              },
+            }}
           >
-            <Avatar
-              src={LOGO}
-              alt={"logo"}
-              sx={{ width: [30, 56], height: [30, 56] }}
-            />
-          </Box>
-          <Box>
-            <Box display={["none", "block"]}>
-              <Stack direction={"row"}>
-                {navLinks.map((ele) => (
-                  <StyledNavLink key={ele.url}>
-                    <StyledLink
-                      to={ele.url}
-                      underline="none"
-                      color={colors.NAV_TEXT}
-                    >
-                      {ele.page}
-                    </StyledLink>
-                  </StyledNavLink>
-                ))}
-              </Stack>
-            </Box>
             <Box
-              display={["block", "none"]}
-              fontSize={"1.5rem"}
-              onClick={() => {
-                setNavbarOpen(!navbarOpen);
-              }}
+              width={["auto", "16.779%"]}
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
             >
-              <RxHamburgerMenu />
+              <Avatar
+                src={LOGO}
+                alt={"logo"}
+                sx={{ width: ["5rem", 20], height: ["5rem", 20] }}
+              />
             </Box>
-            {navbarOpen && (
-              <StyledMobileContainer>
-                <Stack direction={"column"}>
+            <Box>
+              <Box
+                sx={{
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "none",
+                    lg: "block",
+                    xl: "block",
+                  },
+                }}
+              >
+                <Stack direction={"row"}>
                   {navLinks.map((ele) => (
                     <StyledNavLink key={ele.url}>
                       <StyledLink
@@ -126,12 +134,52 @@ const Navbar = () => {
                     </StyledNavLink>
                   ))}
                 </Stack>
-              </StyledMobileContainer>
-            )}
-          </Box>
-        </Stack>
-      </Box>
-    </StyledContainer>
+              </Box>
+              <Box
+                display={["block", "none"]}
+                fontSize={"1.5rem"}
+                onClick={() => {
+                  setNavbarOpen(!navbarOpen);
+                }}
+                sx={{
+                  display: {
+                    xs: "block",
+                    sm: "block",
+                    md: "block",
+                    lg: "none",
+                    xl: "none",
+                  },
+                }}
+              >
+                <RxHamburgerMenu />
+              </Box>
+              {navbarOpen && (
+                <StyledMobileContainer>
+                  <Stack direction={"column"}>
+                    {navLinks.map((ele) => (
+                      <StyledNavLink
+                        key={ele.url}
+                        onClick={() => {
+                          setNavbarOpen(!navbarOpen);
+                        }}
+                      >
+                        <StyledLink
+                          to={ele.url}
+                          underline="none"
+                          color={colors.NAV_TEXT}
+                        >
+                          {ele.page}
+                        </StyledLink>
+                      </StyledNavLink>
+                    ))}
+                  </Stack>
+                </StyledMobileContainer>
+              )}
+            </Box>
+          </Stack>
+        </Box>
+      </StyledContainer>
+    </ThemeProvider>
   );
 };
 
