c132dbdef8e85050d4c6079a5d6ed4824837849c
Imo David <emperorjagz@gmail.com>
2022-12-27 15:30:34 +0100

    feat: slider in progress

--- src/components/ticking-notification/TickingNotification.js
+++ src/components/ticking-notification/TickingNotification.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Box, styled, Typography} from '@mui/material'
-import {Swiper} from "swiper/react";
+import React from "react";
+import { Box, styled, Typography } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { colors } from "../../styles/globals";
 
 // Import Swiper styles
 import "swiper/css";
@@ -9,56 +10,64 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // import required modules
-import {Autoplay, EffectFade} from "swiper";
+import { Autoplay, EffectFade } from "swiper";
 
 const StyledTickingNotificationContainer = styled(Box)({
-    position: 'fixed',
-    bottom: '3%',
-    left: '15px',
-    width: '435px',
-    height: '150px',
-    zIndex: '300'
-})
+  position: "fixed",
+  bottom: "3%",
+  left: "15px",
+  width: "400px",
+  height: "100px",
+  zIndex: "300",
+});
 
-const StyledSlider = styled(Box)({})
+const StyledSlider = styled(Box)({
+  display: "flex",
+  width: "100%",
+  height: "100%",
+});
 
-const TickingNotification = ({data}) => {
-    const filtered_data = data.filter(x => x.image !== null)
-    return (
-        <StyledTickingNotificationContainer>
-            <Swiper
-                effect={"fade"}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
+const TickingNotification = ({ data }) => {
+  const filtered_data = data.filter((x) => x.image !== null);
+  return (
+    <StyledTickingNotificationContainer>
+      <Swiper
+        slidesPerView={1}
+        effect={"fade"}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={2000}
+        modules={[Autoplay, EffectFade]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          {filtered_data.map((item, index) => (
+            <StyledSlider key={index}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: colors.BLACK,
                 }}
-                speed={2000}
-                modules={[Autoplay, EffectFade]}
-            >
-                {
-                    filtered_data.map((item, index) => (
-                        <StyledSlider key={index}>
-                            <Typography variant="h6" sx={{
-                                color: '#000',
-                            }}
-                                        fontWeight={700}
-                                        fontSize={16}
-                                        mb={2}
-                                        ml={2}
-                                        mr={2}
-                                        mt={2}
-                                        textAlign={'center'}
-                                        lineHeight={1.5}
-                            >
-                                {item.title}
-                            </Typography>
-                        </StyledSlider>
-                    ))
-                }
-            </Swiper>
-        </StyledTickingNotificationContainer>
-    );
+                fontWeight={700}
+                fontSize={16}
+                mb={2}
+                ml={2}
+                mr={2}
+                mt={2}
+                textAlign={"center"}
+                lineHeight={1.5}
+              >
+                `{item.first_name}
+                {item.last_name} just won {item.amount}`
+              </Typography>
+            </StyledSlider>
+          ))}
+        </SwiperSlide>
+      </Swiper>
+    </StyledTickingNotificationContainer>
+  );
 };
 
 export default TickingNotification;
